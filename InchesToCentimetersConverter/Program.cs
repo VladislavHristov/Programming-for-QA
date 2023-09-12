48ca679eea429db957719ad642152c466fb726de
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-12 19:11:16 +0300

    Add files via upload

---
+++ InchesToCentimetersConverter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InchesToCentimetersConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double centimeters = inch * 2.54;
+            
+            Console.WriteLine(centimeters);
+        }
+    }
+}
