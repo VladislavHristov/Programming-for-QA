eeb27151234f5851802fc4d23cd2af301788f19e
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-12 18:47:45 +0300

    Add files via upload

---
+++ SquareArea/SquareArea/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int square = a * a;
+
+            Console.WriteLine(square);
+        }
+    }
+}
