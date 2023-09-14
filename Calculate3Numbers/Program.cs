162ac9fdc27794dbcebb198ce10a91a863c45098
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-14 14:02:08 +0300

    Add files via upload

---
+++ Calculate3Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Channels;
+
+namespace Calculate3Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z. 
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine()); 
+            int sum1 = (x + y) * z;
+            int sum2 = (x * y) + (y * z);
+
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+        }
+    }
+}
---
+++ Calculate3Numbers/README.md
@@ -0,0 +1,9 @@
+﻿# Programming-for-QA
+Write a C# Sharp program that takes three numbers (x,y,z) as input and outputs (x+y).z and x.y + y.z. 
+Test Data:
+Enter first number - 5
+Enter second number - 6
+Enter third number - 7
+
+Expected Output:
+Result of specified numbers 5, 6 and 7, (x+y).z is 77 and x.y + y.z is 72
