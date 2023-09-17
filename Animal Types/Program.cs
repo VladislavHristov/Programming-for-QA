d4fb64aa045f79c192a554e3aa1a88134a8403e4
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-17 21:09:32 +0300

    Add files via upload

---
+++ Animal Types/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Animal_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            switch (animal)
+            {
+                case"dog": 
+                    Console.WriteLine("mammal"); 
+                    break;
+                case"crododile": 
+                    Console.WriteLine("reptile");
+                    break;
+                case"tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+                case"snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+
+
+            }
+            
+        }
+    }
+}
