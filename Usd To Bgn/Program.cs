3d8bc52ae8b643a78d7bba205785a74ef087e7dc
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-17 21:12:33 +0300

    Add files via upload

---
+++ Usd To Bgn/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Usd_To_Bgn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79549;
+            Console.WriteLine(bgn);
+            
+        }
+    }
+}
