52c2612f97b1e605c0ce87bba248576342e7d23e
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-20 20:47:36 +0300

    Add files via upload

---
+++ Radians to Degrees/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian = double.Parse(Console.ReadLine());
+            double deg = radian * 180 / Math.PI;
+            
+            Console.WriteLine(deg);
+        }
+    }
+}
