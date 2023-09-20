ffd90b410afcf01271030f1313aa09ae40eed7c0
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-20 20:42:50 +0300

    Add files via upload

---
+++ Redecorating/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Redecorating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	Protective nylon -1.50 BGN per square meter
+            //•	Paint - 14.50 BGN per liter
+            //•	Paint thinner -5.00 BGN per liter
+
+            //1.Required amount of nylon(in sq.m.) - an integer number in the range[1... 100]
+            //2.Required amount of paint(in liters) - an integer number in the range[1... 100]
+            //3.Quantity of thinner(in liters) -integer number in the range[1... 30]
+            //4.Hours needed for the craftsmen to do the work -an integer number in the range[1... 9]
+
+
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
