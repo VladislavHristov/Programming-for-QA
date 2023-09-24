b610d6f5fc5bc44c90152fd75812c5ed93cdda91
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-24 14:22:07 +0300

    Add files via upload

---
+++ Mandatory Literature/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Mandatory_Literature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());  
+            int pagesPerHour = int.Parse(Console.ReadLine());     
+            int days = int.Parse(Console.ReadLine());
+
+            int totalHours = pages / pagesPerHour;
+            int requiredHours = totalHours / days;
+
+            Console.WriteLine(requiredHours);
+        }
+    }
+}
