76ed31c8ca1ab4b93cfc168e567a8b8ba35e30c6
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-09-27 21:48:55 +0300

    Add files via upload

---
+++ Deposit Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount = double.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());  
+            double rate = double.Parse(Console.ReadLine());
+            double interest = amount * (rate / 100);
+            double forOneMonth = interest / 12;
+            double lastamount = amount + month * forOneMonth;
+
+            //lastamount = deposited amount + term of deposit * (deposited amount * annual interest rate) / 12
+            Console.WriteLine(lastamount);
+
+
+
+            
+        }
+    }
+}
