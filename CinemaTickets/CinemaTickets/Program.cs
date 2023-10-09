036d11cba9e4c70bd2d85b9bfff8a6de1a0b2f78
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-09 21:22:40 +0300

    Add files via upload

---
+++ CinemaTickets/CinemaTickets/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();   
+            int rows = int.Parse(Console.ReadLine());   
+            int columns = int.Parse(Console.ReadLine());
+
+            //•	Premiere – premiere screening, at a price of 12.00 leva.
+            //•	Normal – standard screening, at a price of 7.50 leva.
+            //•	Discount – screening for children and students at a reduced price of 5.00 leva.
+            double income = 0.0;
+            if (type == "Premiere")
+            {
+                income = rows * columns * 12;
+            }else if (type == "Normal")
+            {
+                income = rows * columns * 7.50;
+            }else if (type == "Discount")
+            {
+                income = rows * columns * 5.00;
+            }
+
+            Console.WriteLine($"{income:F2} leva");
+        }
+    }
+}
