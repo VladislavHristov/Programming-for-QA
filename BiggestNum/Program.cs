fb9615b5c2a6e4f08a9578ded3e04b26d2484d06
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-12 22:46:39 +0300

    Add files via upload

---
+++ BiggestNum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BiggestNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int biggest = int.MinValue;
+
+
+            for (int i =1;i <=n; i++)
+            {
+              int newNum=int.Parse(Console.ReadLine());
+               // biggest = newNum;
+                
+                if (newNum>biggest )
+                {
+                    biggest = newNum;
+                }
+                
+            }
+            Console.WriteLine(biggest);
+
+        }
+    }
+}
