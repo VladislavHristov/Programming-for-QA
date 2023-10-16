2c7788619d93af31722cd594e7c018103c9ecd8b
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-16 15:53:02 +0300

    Add files via upload

---
+++ MultiTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MultiTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 1; i <= 10; i++)
+
+            {
+                
+                result = n * i;
+                Console.WriteLine($"{n} x {i} = {result}");
+              
+            }
+        }
+    }
+}
