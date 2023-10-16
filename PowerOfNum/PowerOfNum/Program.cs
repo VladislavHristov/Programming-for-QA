1a5b0c14fa9638283c800b51fcbd26be88ea8033
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-16 15:55:41 +0300

    Add files via upload

---
+++ PowerOfNum/PowerOfNum/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PowerOfNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = 1;
+            for(int i = 0;i<power;i++)
+            {
+                result *= num;
+                
+            }
+            Console.WriteLine(result);
+        }
+        
+        
+    }
+}
