e14fa2e59b45b40e20ce42d86acb381332803529
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-24 10:57:31 +0300

    Add files via upload

---
+++ SpecialNum/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SpecialNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isSpecial = true;
+            int originalNum = number;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (originalNum % digit != 0)
+                {
+                    isSpecial = false;
+                    break;
+                }
+                number = number / 10;
+            }
+            
+            if (isSpecial==true) 
+            { 
+            Console.WriteLine($"{originalNum} is special");
+            }else
+            {
+                Console.WriteLine($"{originalNum} is not special");
+            }
+        }
+    }
+}
