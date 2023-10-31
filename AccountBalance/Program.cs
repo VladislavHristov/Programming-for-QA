f54325e457fc395fd7688e4a792fcfffc86bf85d
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-10-31 11:45:29 +0200

    Add files via upload

---
+++ AccountBalance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = 0;
+
+            
+
+            while (true)
+            {
+                
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "end")
+                    break;
+
+                if (double.TryParse(input, out double transaction))
+                {
+                    balance += transaction;
+                    if (transaction >= 0)
+                        Console.WriteLine($"Increase: {transaction:F2}");
+                    else
+                        Console.WriteLine($"Decrease: {Math.Abs(transaction):F2}");
+                }
+                
+            }
+
+            Console.WriteLine($"Balance: {balance:F2}");
+        }
+    }
+}
