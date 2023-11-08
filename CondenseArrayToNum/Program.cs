be253efd093e35a99afa8a2d523e2638e8bb0550
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-11-08 11:07:29 +0200

    Add files via upload

---
+++ CondenseArrayToNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        string input = Console.ReadLine();
+        int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+        while (numbers.Length > 1)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+            numbers = condensed;
+        }
+
+        Console.WriteLine(numbers[0]);
+    }
+}
