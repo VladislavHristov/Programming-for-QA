ed0a23aeea71c3011c162f18b983314e68f84d23
Vladislav Hristov <136968279+VladislavHristov@users.noreply.github.com>
2023-11-08 11:10:11 +0200

    Add files via upload

---
+++ GaussTrick/GaussTrick/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        string input = Console.ReadLine();
+        List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+        int first = 0;
+        int last = numbers.Count - 1;
+        List<int> result = new List<int>();
+
+        while (first <= last)
+        {
+            if (first == last)
+            {
+                result.Add(numbers[first]);
+            }
+            else
+            {
+                result.Add(numbers[first] + numbers[last]);
+            }
+
+            first++;
+            last--;
+        }
+
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
